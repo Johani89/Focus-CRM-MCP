1308cc36d65583ceb98660a519f5950d69669dc7
Johani89 <Johanielhadi89@gmail.com>
2026-08-29 15:41:41 -0400

    Support separate Reynolds authentication host

--- app/config.py
+++ app/config.py
@@ -5,8 +5,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', extra='ignore')
     app_name: str = 'FOCUS CRM MCP'
-    app_version: str = '0.1.0'
+    app_version: str = '0.1.1'
     focus_base_url: str = ''
+    focus_auth_base_url: str = ''
     focus_username: str = ''
     focus_password: str = ''
     focus_dealer_id: str = ''
@@ -14,7 +15,7 @@ class Settings(BaseSettings):
     focus_auth_mode: str = 'form'
     focus_username_field: str = 'username'
     focus_password_field: str = 'password'
-    focus_login_path: str = '/login'
+    focus_login_path: str = '/Account/LogOn'
     focus_session_cookie: str = ''
     focus_csrf_header: str = ''
     focus_csrf_token: str = ''
@@ -32,8 +33,12 @@ class Settings(BaseSettings):
     focus_verify_tls: bool = True
 
     @property
+    def auth_base_url(self) -> str:
+        return self.focus_auth_base_url or self.focus_base_url
+
+    @property
     def configured(self) -> bool:
-        return bool(self.focus_base_url and self.focus_username and self.focus_password)
+        return bool(self.focus_base_url and self.auth_base_url and self.focus_username and self.focus_password)
 
 
 @lru_cache
